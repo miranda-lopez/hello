736481e7461559232269698da0aeab0e22dc76b4
Miranda <mlopeztovar2@huskers.unl.edu>
2021-08-26 13:58:49 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * autor: Miranda Lopez 
+ * Date: 2021/08/24
+ * 
+ * A simple hello world program in C
+ * 
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+ 
+ int main(int argc, char **argv) {
+     
+     printf("hello world, I'm Miranda Lopez!\n");
+     printf("My major is Computer Science\n");
+     
+     return 0;
+     
+ }
