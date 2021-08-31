a3c46be05a025f9a6205fa6d5ea461d07f02706a
miranda-lopez <79167230+miranda-lopez@users.noreply.github.com>
2021-08-30 20:42:32 -0500

    Update hello.c

--- hello.c
+++ hello.c
@@ -8,11 +8,14 @@
  #include <stdlib.h>
  #include <stdio.h>
  
- int main(int argc, char **argv) {
-     
+//this is a the "main function" 
+int main(int argc, char **argv) {
+ 
+     //print my name and my major
      printf("hello world, I'm Miranda Lopez!\n");
      printf("My major is Computer Science\n");
-  
+ 
+     //print collab name and major
      printf("Hello World, I'm Ngan Nguyen!\n");
      printf("My major is Computer Science.\n);
      
